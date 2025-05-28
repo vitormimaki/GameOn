ad7e5d73781ecbd014ea28753d52f05360f14ac4
Vitor Mimaki <vitormimaki@gmail.com>
2025-05-28 13:34:15 -0300

    Profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -41,12 +41,17 @@ export default function Profile() {
           <Text style={styles.userName}>Nome do Usuário</Text>
           <Text style={styles.description}>Descrição</Text>
           
-          <View style={{ width: "80%", paddingHorizontal: 20, color: "#333", backgroundColor: "gray", borderRadius: 30, flexDirection: "row", justifyContent: "space-around", alignItems: "center", marginBottom: 20, height: 50 }}>
-            <View style={{ width: "50%", paddingHorizontal: 20, color: "#333", backgroundColor: "blue", borderRadius: 30, flexDirection: "row", justifyContent: "space-around", alignItems: "center", marginBottom: 20, height: 50 }}>
+          <View style={{ width: "80%", color: "#333", backgroundColor: "gray", borderWidth: 2, borderColor: 'gray', borderRadius: 30, flexDirection: "row", justifyContent: "flex-start", alignItems: "center", marginBottom: 20, height: 50 }}>
+            <View style={{ width: "50%", color: "#333", backgroundColor: "white", borderRadius: 30, flexDirection: "row", justifyContent: "space-around", alignItems: "center", height: "100%" }}>
                 <Text>Posts</Text>
             </View>
-            <Text>Informações</Text>
+            <View style={{ width: "50%", color: "#333", backgroundColor: "gray", borderRadius: 30, flexDirection: "row", justifyContent: "space-around", alignItems: "center", height: "100%" }}>
+                <Text>Informações</Text>
+            </View>
           </View>
+
+          <View style={[styles.form, { width: "80%", padding: 20, borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }]} />
+
         </View>
       </View>
     </View>
