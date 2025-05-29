d130a5146c4852fe0284fa606721ea06938553b7
Vitor Mimaki <vitormimaki@gmail.com>
2025-05-29 10:00:59 -0300

    Card

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,10 +3,40 @@ import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import { Icon } from "@rneui/themed";
 import { Stack } from "expo-router";
+import SwipeCard from "../../components/Card";
 
 export default function Home() {
+
+    const exampleProfiles = [
+    {
+        name: "Alice",
+        age: 25,
+        image: require('../../assets/images/icon.jpeg'),
+        tags: [
+        { label: "Adventurous", color: "#ff6347" },
+        { label: "Traveler", color: "#4682b4" },
+        ],
+    },
+    {
+        name: "Bob",
+        age: 30,
+        image: require('../../assets/images/icon.jpeg'),
+        tags: [
+        { label: "Gamer", color: "#32cd32" },
+        { label: "Tech Enthusiast", color: "#ffa500" },
+        ],
+    },
+    ];
+
+    const handleSwipeRight = (profile) => {
+        console.log(`Swiped right on ${profile.name}`);
+    };
+    const handleSwipeLeft = (profile) => {
+        console.log(`Swiped left on ${profile.name}`);
+    };
+
     return (
-        <View style={styles.container}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Stack.Screen
                 options={{
                     headerTitle: "Game On",
@@ -14,9 +44,14 @@ export default function Home() {
                     headerStyle: styles.header,
                 }}
             />
-            <Text style={styles.subHeader} accessibilityRole="text" accessibilityLabel="Bem-vindo ao Game On!">
-                Bem-vindo ao Game On!
-            </Text>
+            {exampleProfiles.map((profile, index) => (
+                <SwipeCard
+                    key={index}
+                    profile={profile}
+                    onSwipeRight={handleSwipeRight}
+                    onSwipeLeft={handleSwipeLeft}
+                />
+            ))}
         </View>
     );
 }
